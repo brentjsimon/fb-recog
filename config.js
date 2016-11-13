156a959818c3c1fca9a3e656865aadc5d571c4fb
brentjsimon <brentjohn.simon@sjsu.edu>
2016-11-12 16:07:32 -0800

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 module.exports = {
     port: 3000,
-    client_id: 'YOUR_APP_ID',
-    client_secret: 'YOUR_APP_SECRET',
+    client_id: '1812189635726971',
+    client_secret: '768218a61edd6f08d7a30f831bdb59ad',
     scope: 'publish_actions',
     cookies: 'datr=YOUR_COOKIE; lu=YOUR_COOKIE; ...; act=YOUR_COOKIE; presence=YOUR_COOKIE', // put all of your cookies here
     req_params: '__user=...&...' // read README.md
